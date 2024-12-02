6935b759437e2a23497e91d8e08c1c7fa3c370e7
Marta <mlopron829@g.educaand.es>
2024-12-02 10:43:59 +0100

    codigo criptografia asimetrica

---
+++ 5_criptografia/code/cripto_asimetrica.py
@@ -0,0 +1,45 @@
+from ecies import encrypt, decrypt
+from ecies.utils import generate_eth_key, generate_key
+
+
+# Generacion claves publica y privada
+eth_k = generate_eth_key()
+clave_privada = eth_k.to_hex()
+clave_publica = eth_k.public_key.to_hex()
+
+print("clave publica:", clave_publica)
+print("clave privada:", clave_privada)
+
+# Cifrado con clave publica
+mensaje = b'Esto es un mensaje secreto'
+mensage_encriptado = encrypt(clave_publica, mensaje)
+print("Mensaje encriptado:", mensage_encriptado.hex())
+
+# Descifrado con clave privada
+mensaje_desencriptado = decrypt(clave_privada, mensage_encriptado)
+print("Mensaje desencriptado:", mensaje_desencriptado)
+
+
+# Probando a descifrar con la misma clave publica
+try:
+    mensaje_desencriptado = decrypt(clave_publica, mensage_encriptado)
+    print("Mensaje desencriptado:", mensaje_desencriptado)
+except:
+    print("No es posible descifrar con la misma clave publica. Da error")
+
+# Probando a descifrar con una clave privada generada nueva 
+# (sin relacion con la publica original)
+eth_k = generate_eth_key()
+clave_privada = eth_k.to_hex()
+clave_publica = eth_k.public_key.to_hex()
+
+try:
+    mensaje_desencriptado = decrypt(clave_publica, mensage_encriptado)
+    print("Mensaje desencriptado:", mensaje_desencriptado)
+except:
+    print("No es posible descifrar con una clave privada distinta a la relacionada con la publica original. Da error")
+
+
+
+
+
